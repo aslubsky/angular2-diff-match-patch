6f4545d410e33b7472edd47c94f88b43fd919bd7
Alex Slubsky <aslubsky@gmail.com>
2016-12-22 14:43:50 +0200

    add AOT

---
+++ ng2-diff.module.ts
@@ -0,0 +1,15 @@
+import {LineDiffComponent} from './src/line-diff.component';
+
+import {NgModule} from '@angular/core';
+
+@NgModule({
+    exports: [
+        LineDiffComponent
+    ],
+    declarations: [
+        LineDiffComponent
+    ],
+    imports: []
+})
+export class Ng2DiffModule {
+}
